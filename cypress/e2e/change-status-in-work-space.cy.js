6525bdc2b1bc5819bf7b9a61db6859be2674f2bc
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-05 01:26:35 +0700

    Вынес данные для теста смены статуса рабочего пространства в отдельный json файл

--- cypress/e2e/change-status-in-work-space.cy.js
+++ cypress/e2e/change-status-in-work-space.cy.js
@@ -1,41 +1,45 @@
 describe('Изменение статуса рабочего пространства', () => {
     it('Прошёл стажировку', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('testerInstitution')
-        
-        cy.get('.form-input--password').type('Password1')
+            cy.get('.form-input--text').type(data.login_rabotodatel)
+            
+            cy.get('.form-input--password').type(data.password_rabotodatel)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(1000)
-        cy.get(':nth-child(5) > .menu-item__item-name').click()
+            cy.wait(1000)
+            cy.get(':nth-child(5) > .menu-item__item-name').click()
 
-        cy.wait(2000)
-        cy.get('.infinite-loader > :nth-child(2) > .button').click()
+            cy.wait(2000)
+            cy.get('.infinite-loader > :nth-child(2) > .button').click()
 
-        cy.wait(2000)
-        cy.get('.status-open__buttons > :nth-child(1)').click()
+            cy.wait(2000)
+            cy.get('.status-open__buttons > :nth-child(1)').click()
+        })
     });
 
     it('Непрошёл стажировку', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('testerInstitution')
-        
-        cy.get('.form-input--password').type('Password1')
+            cy.get('.form-input--text').type(data.login_rabotodatel)
+            
+            cy.get('.form-input--password').type(data.password_rabotodatel)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(1000)
-        cy.get(':nth-child(5) > .menu-item__item-name').click()
+            cy.wait(1000)
+            cy.get(':nth-child(5) > .menu-item__item-name').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(5) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(5) > .button').click()
 
-        cy.wait(2000)
-        cy.get('.status-open__buttons > :nth-child(2)').click()
+            cy.wait(2000)
+            cy.get('.status-open__buttons > :nth-child(2)').click()
+        })
     })
 })
