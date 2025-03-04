d54f75fa384bfd94b6e377fc760cec247bf22435
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-05 00:34:27 +0700

    Вынес данные дляч теста странице поиска стажировки в отдельный json файл

--- cypress/e2e/search-Internships.cy.js
+++ cypress/e2e/search-Internships.cy.js
@@ -1,74 +1,79 @@
 describe('Поиск стажировок', () => {
     it('Поиск', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('TsumbalM')
-        
-        cy.get('.form-input--password').type('N1kt0-js')
+            cy.get('.form-input--text').type(data.login)
+            
+            cy.get('.form-input--password').type(data.password)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
+            cy.wait(2000)
+            cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
 
-        cy.get('.form-input--text').type('Дворник')
+            cy.get('.form-input--text').type(data.search_title_internship)
 
-        cy.get('div.search-input__field > .button').click()
+            cy.get('div.search-input__field > .button').click()
 
-        cy.wait(2000)
-        cy.get('.custom-modal-mobile__buttons > .button').click()
-
+            cy.wait(2000)
+            cy.get('.custom-modal-mobile__buttons > .button').click()
+        })
     });
     
     it('Выбор оплаты', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('TsumbalM')
-        
-        cy.get('.form-input--password').type('N1kt0-js')
+            cy.get('.form-input--text').type(data.login)
+            
+            cy.get('.form-input--password').type(data.password)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
+            cy.wait(2000)
+            cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(2) > .radio-component__input').click()
+            cy.wait(2000)
+            cy.get(':nth-child(2) > .radio-component__input').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .radio-component__input').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .radio-component__input').click()
 
-        cy.wait(2000)
-        cy.get('.custom-modal-mobile__buttons > .button').click()
+            cy.wait(2000)
+            cy.get('.custom-modal-mobile__buttons > .button').click()
+        })
     });
 
     it('Выбор занятости', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('TsumbalM')
-        
-        cy.get('.form-input--password').type('N1kt0-js')
+            cy.get('.form-input--text').type(data.login)
+            
+            cy.get('.form-input--password').type(data.password)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
-        
-        cy.wait(2000)
-        cy.get('.form-select__selected').click()
+            cy.wait(2000)
+            cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
+            
+            cy.wait(2000)
+            cy.get('.form-select__selected').click()
 
-        cy.wait(2000)
-        cy.get('.form-select__items > :nth-child(3)').click()
+            cy.wait(2000)
+            cy.get('.form-select__items > :nth-child(3)').click()
 
-        cy.wait(2000)
-        cy.get('.form-select__selected').click()
+            cy.wait(2000)
+            cy.get('.form-select__selected').click()
 
-        cy.get('.form-select__items > :nth-child(2)').click()
+            cy.get('.form-select__items > :nth-child(2)').click()
 
-        cy.wait(2000)
-        cy.get('.custom-modal-mobile__buttons > .button').click()
+            cy.wait(2000)
+            cy.get('.custom-modal-mobile__buttons > .button').click()
+        })
     })
 })
