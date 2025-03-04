1983639d3711f25a0b0164dea2c4c8bde4d8a24c
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-05 01:02:24 +0700

    Вынес данные для теста взаимодействия внутри рабочего пространства в отдельный json файл

--- cypress/e2e/comunicate-in-work-space.cy.js
+++ cypress/e2e/comunicate-in-work-space.cy.js
@@ -1,24 +1,26 @@
 describe('Взаимодействие внутри пространства', () => {
     it('Оставление коментария', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('testerInstitution')
+            cy.get('.form-input--text').type(data.login_rabotodatel)
         
-        cy.get('.form-input--password').type('Password1')
+            cy.get('.form-input--password').type(data.password_rabotodatel)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(1000)
-        cy.get(':nth-child(5) > .menu-item__item-name').click()
+            cy.wait(1000)
+            cy.get(':nth-child(5) > .menu-item__item-name').click()
 
-        cy.wait(2000)
-        cy.get('.infinite-loader > :nth-child(2) > .button').click()
+            cy.wait(2000)
+            cy.get('.infinite-loader > :nth-child(2) > .button').click()
 
-        cy.get('.form-area').click()
-        cy.get('.form-area').type('dfhbdzfbdfbdzfb')
+            cy.get('.form-area').click()
+            cy.get('.form-area').type(data.comment)
 
-        cy.wait(2000)
-        cy.get('.comment-textarea__buttons > :nth-child(2)').click()
+            cy.wait(2000)
+            cy.get('.comment-textarea__buttons > :nth-child(2)').click()
+        })
     })
 })
