11991a5c1e87384af0a8233cbee4571aca45f77c
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-05 00:25:40 +0700

    Вынес данные для теста создания стажировки в отдельный json файл

--- cypress/e2e/create-Internships.cy.js
+++ cypress/e2e/create-Internships.cy.js
@@ -1,43 +1,45 @@
 describe('Создание стажировки работодателем', () => {
     it('Создание стажировки', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('testerInstitution')
-        
-        cy.get('.form-input--password').type('Password1')
+            cy.get('.form-input--text').type('testerInstitution')
+            
+            cy.get('.form-input--password').type('Password1')
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(1000)
-        cy.get(':nth-child(8) > .menu-item__item-name').click()
+            cy.wait(1000)
+            cy.get(':nth-child(8) > .menu-item__item-name').click()
 
-        cy.wait(1000)
-        cy.get('[data-v-e4f6348f=""][data-v-4849dea2=""] > .vacancies-block > .vacancies-block__filters-wrapper > .button').click()
+            cy.wait(1000)
+            cy.get('[data-v-e4f6348f=""][data-v-4849dea2=""] > .vacancies-block > .vacancies-block__filters-wrapper > .button').click()
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--').type('Тестрирование игры Maincraft')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--').type(data.internship_title)
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(2) > .form-select > :nth-child(2) > .form-select__selected').click()
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(2) > .form-select > :nth-child(2) > .form-select__selected').click()
 
-        cy.get('.form-select__items > :nth-child(1)').click()
+            cy.get('.form-select__items > :nth-child(1)').click()
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(3) > .form-select > :nth-child(2) > .form-select__selected').click()
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(3) > .form-select > :nth-child(2) > .form-select__selected').click()
 
-        cy.get('.form-select__items > :nth-child(1)').click()
+            cy.get('.form-select__items > :nth-child(1)').click()
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(4) > .form-control--responsive > .form-input--date').click()
-        cy.wait(8000)
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(4) > .form-control--responsive > .form-input--date').click()
+            cy.wait(8000)
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(5) > .form-control--responsive > .form-input--date').click()
-        cy.wait(10000)
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(5) > .form-control--responsive > .form-input--date').click()
+            cy.wait(10000)
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(6) > .form-control > .form-area').type('dfbzdfbzdfbzdfbzdfb')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(6) > .form-control > .form-area').type(data.internship_requirements)
 
-        cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(7) > .form-control > .form-area').type('dfbzdfbzdffbzdfbzdffbzdfb')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(7) > .form-control > .form-area').type(data.internship_responsibilities)
 
-        cy.get('.vacancy-add-form-wrapper > .form > .form__buttons > .buttons > .button').click()
+            cy.get('.vacancy-add-form-wrapper > .form > .form__buttons > .buttons > .button').click()
 
-        cy.wait(1000)
-        cy.get(':nth-child(1) > .internship-item__info-wrapper > .internship-item__footer-wrapper > .vacancy-footer > .vacancy-footer__button-wrapper > .button__background-color-green').click()
+            cy.wait(1000)
+            cy.get(':nth-child(1) > .internship-item__info-wrapper > .internship-item__footer-wrapper > .vacancy-footer > .vacancy-footer__button-wrapper > .button__background-color-green').click()
+        })
     })
 })
