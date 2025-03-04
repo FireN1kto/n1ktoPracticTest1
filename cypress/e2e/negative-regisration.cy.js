b76e6cca3aa533071d3a710cdb0016da416dc077
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-04 23:55:58 +0700

    Вынес данные для тестов регистрации в отдельный json файл

--- cypress/e2e/negative-regisration.cy.js
+++ cypress/e2e/negative-regisration.cy.js
@@ -1,27 +1,35 @@
 describe('Регистрация', () => {
     it('Негативная регистрация пользователя - неправильный логин', () => {
-        cy.visit('https://dev.profteam.su/registration')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/registration')
 
-        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('ЦымабалМ')
+            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.login_fail)
+        })
     });
 
     it('Неагтивная регистрация пользователя - невалидная почта', () => {
-        cy.visit('https://dev.profteam.su/registration')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/registration')
 
-        cy.get('.form-input--email').type('maksimcymbal77gmail.com')
+            cy.get('.form-input--email').type(data.email_fail)
+        })
     });
 
     it('Негативная регистрация пользователя - неправильный пароль', () => {
-        cy.visit('https://dev.profteam.su/registration')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/registration')
 
-        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('n1kt0 js')
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.password_fail)
+        })
     });
 
     it('Негативная регистрация пользователя - несовпадение паролей', () => {
-        cy.visit('https://dev.profteam.su/registration')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/registration')
 
-        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('N1kt0-js')
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.password)
 
-        cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type('n1kt0-js')
+            cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.password_fail)
+        })
     });
 })
--- cypress/e2e/test-regisrtation.cy.js
+++ cypress/e2e/test-regisrtation.cy.js
@@ -1,27 +1,29 @@
 describe('Регистрация', () => {
     it('Положительная регистрация пользователя', () => {
-        cy.visit('https://dev.profteam.su/registration');
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/registration');
 
-        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('TsumbalM')
+            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.login)
 
-        cy.get('.form-input--email').type('maksimcymbal77@gmail.com')
+            cy.get('.form-input--email').type(data.email)
 
-        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('N1kt0-js')
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.password)
 
-        cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type('N1kt0-js')
-        
-        cy.wait(2000)
-        cy.get('.form__buttons > :nth-child(4)').click()
+            cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.password)
+            
+            cy.wait(2000)
+            cy.get('.form__buttons > :nth-child(4)').click()
 
-        cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type('Цымбал')
+            cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type(data.surname)
 
-        cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type('Максим')
-
-
-        cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type('Вадимович')
+            cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type(data.name)
 
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type(data.patronymic)
+
+
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
+        })
     })
 })
