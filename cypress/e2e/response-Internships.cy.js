37d464bce526dac23b7aceeec47b3e7ed60fd365
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-05 00:41:19 +0700

    Вынес данные для теста отклика на стажировку студентом в отдельный json файл

--- cypress/e2e/response-Internships.cy.js
+++ cypress/e2e/response-Internships.cy.js
@@ -1,18 +1,20 @@
 describe('Поиск стажировок', () => {
     it('Поиск', () => {
-        cy.visit('https://dev.profteam.su/login')
+        cy.fixture('cypresssTest').then(data =>{
+            cy.visit('https://dev.profteam.su/login')
 
-        cy.get('.form-input--text').type('TsumbalM')
+            cy.get('.form-input--text').type(data.login)
         
-        cy.get('.form-input--password').type('N1kt0-js')
+            cy.get('.form-input--password').type(data.password)
 
-        cy.wait(2000)
-        cy.get(':nth-child(3) > .button').click()
+            cy.wait(2000)
+            cy.get(':nth-child(3) > .button').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
+            cy.wait(2000)
+            cy.get(':nth-child(1) > .header__nav > [href="/internships"] > .header__label').click()
 
-        cy.wait(2000)
-        cy.get(':nth-child(1) > .internship-item__info-wrapper > .internship-item__footer-wrapper > .vacancy-footer > .vacancy-footer__button-wrapper > .button__background-color-green').click()
+            cy.wait(2000)
+            cy.get(':nth-child(1) > .internship-item__info-wrapper > .internship-item__footer-wrapper > .vacancy-footer > .vacancy-footer__button-wrapper > .button__background-color-green').click()
+        })
     });
 })
